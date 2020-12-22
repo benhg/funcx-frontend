1911c7896df7627c83e7676bd32d0937c5218844
Ben Glick <glick@glick.cloud>
2020-12-21 21:53:18 -0800

    add croc stuff

--- funcx_frontend/transfer.py
+++ funcx_frontend/transfer.py
@@ -6,19 +6,22 @@ from funcx_frontend.execution import run_console_cmd
 def upload_file(endpoint_id,
                        local_path=None,
                        remote_path="~",
-                       username=None,
                        force=False):
     """
     Upload a file or directory to endpoint.
 
     Uses Croc as a transport layer
 
-    :param local_path: Path where file should be saved
-    :param remote_path: Path where file is currently
-    :param username: Remote host username
+    :param local_path: Path where file is currently saved
+    :param remote_path: Path where file should be saved
     :param force: Do not ask user to overwrite existing files
     """
-    pass
+    passphrase = f"croc-upload-{random.randint(0, 100000)}"
+    output = subprocess.Popen(
+        ["croc", "send", "--code", passphrase, local_path])
+    print(passphrase)
+    run_console_cmd(f"croc --yes {passphrase} --out {remote_path}")
+    print(f"{local_path} has been uploaded to {remote_path} on remote endpoint")
 
 
 def download_file(endpoint_id,
@@ -36,4 +39,9 @@ def download_file(endpoint_id,
     :param username: Remote host username
     :param force: Do not ask user to overwrite existing files
     """
-    pass
+    passphrase = f"croc-upload-{random.randint(0, 100000)}"
+    f"croc --yes {passphrase} --out {remote_path}"
+    output = subprocess.Popen(
+        ["croc", "--yes", passphrase, "--out", local_path])
+    run_console_cmd(f"croc send --code {passprhase} {remote_path}")
+    print(f"{remote_path} has been downloaded to {local_path} on current computer")
